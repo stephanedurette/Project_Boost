d6f3e3fd9b5df38102fe82e9d01a60fe6e1b0dd2
stephanedurette <stephanedurette@rogers.com>
2020-03-30 16:24:42 -0700

    Rocket Logic
    Rocket Logic

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Rocket : MonoBehaviour{
+    
+    [SerializeField] KeyCode rotateLeft = KeyCode.A;
+    [SerializeField] KeyCode rotateRight = KeyCode.D;
+    [SerializeField] KeyCode thrustKey = KeyCode.Space;
+    [SerializeField] float thrustForce;
+    [SerializeField] float rotateSpeed;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    private void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void FixedUpdate(){
+        ProcessInput();
+    }
+
+    void ProcessInput(){
+        if (Input.GetKey(thrustKey)){
+            rigidBody.AddRelativeForce(Vector3.up * thrustForce * Time.deltaTime);
+            if (!audioSource.isPlaying) audioSource.Play();
+        } else {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(rotateLeft)) transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+        if (Input.GetKey(rotateRight)) transform.Rotate(-Vector3.forward * rotateSpeed * Time.deltaTime);
+        
+    }
+
+}
